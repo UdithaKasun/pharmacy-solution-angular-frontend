4715155e36fc5aaaab32d1e85acc2ead591290f3
UdithaKasun <udithakasun311@gmail.com>
2017-11-04 01:29:22 +0530

    Post Request for Drugs Created

--- src/app/pharm-drug/pharm-drug-add/pharm-drug-add.component.ts
+++ src/app/pharm-drug/pharm-drug-add/pharm-drug-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DrugService } from '../../shared/services/drug.service';
+import { Drug } from '../../shared/models/drug';
 
 @Component({
   selector: 'app-pharm-drug-add',
@@ -12,6 +13,8 @@ export class PharmDrugAddComponent implements OnInit {
     drugAddForm: FormGroup;
     submitted: boolean;
     drugCategories : any[];
+    drugType = "Tablet";
+    drugCategory = "";
 
     constructor(private drugService : DrugService){
       
@@ -26,13 +29,34 @@ export class PharmDrugAddComponent implements OnInit {
       
     }
 
+    saveDrug(){
+      var drug = new Drug();
+      drug.drug_name = this.drugAddForm.get('drugName').value;
+      drug.drug_price = this.drugAddForm.get('drugPrice').value;
+      drug.drug_current_quantity = this.drugAddForm.get('drugInitialQuantity').value;
+      drug.drug_status_reorder = this.drugAddForm.get('drugReorderQuantity').value;
+      drug.drug_remarks = "";
+      drug.drug_create_user = "USER_001";
+      drug.drug_active = true;
+      drug.drug_unit = this.drugAddForm.get('drugType').value;
+      drug.drug_category_id = this.drugAddForm.get('drugCategory').value;
+      console.log(drug);
+      this.drugService.saveDrug(drug)
+      .subscribe(data => {
+        console.log(data);
+      })
+      
+    }
+
 
     ngOnInit(): void {
       this.drugAddForm = new FormGroup({
         drugName: new FormControl('', Validators.required),
         drugPrice: new FormControl('',[Validators.required,Validators.pattern('[0-9]+.[0-9][0-9]')]),
         drugInitialQuantity: new FormControl('',[Validators.required,Validators.pattern('[0-9]+')]),
-        drugReorderQuantity: new FormControl('',[Validators.required,Validators.pattern('[0-9]+')])  
+        drugReorderQuantity: new FormControl('',[Validators.required,Validators.pattern('[0-9]+')]),
+        drugCategory: new FormControl('', Validators.required), 
+        drugType: new FormControl('', Validators.required),  
       });
       this.submitted = false;
       this.loadDrugCategories();
@@ -40,7 +64,7 @@ export class PharmDrugAddComponent implements OnInit {
   
   
     onSubmit() {
-      
+      this.saveDrug();
     }
     addNewEmployeeAddress() {
       this.drugAddForm.reset();
--- src/app/shared/services/drug.service.ts
+++ src/app/shared/services/drug.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs/Observable';
 import { DrugCategory } from '../models/drugcategories';
+import { Drug } from '../models/drug';
 @Injectable()
 export class DrugService {
 
@@ -10,6 +11,13 @@ export class DrugService {
   //Load Drug Categories
   loadDrugCategories() : Observable<any> {
     return this.apiService.get('/drugs/categories/all')
+    .map(data => {
+      return data;
+    });
+  }
+
+  saveDrug(drug : Drug) : Observable<any> {
+    return this.apiService.post('/drugs', { drug : drug })
     .map(data => {
       return data;
     });
